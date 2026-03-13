eb48c77d07bd2c387d9194064e490e3bdf9f76c9
PolPanyella <pol.panyella@enti.cat>
2026-03-13 17:40:54 +0100

    xgboost y regresion logistica pruebas

---
+++ src/models/boostgx.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+
+# cargar datos
+data = pd.read_csv("src/data/raw/historial_pacientes/datos_combinados_completos.csv")
+# separar variables
+
+data["cancer"] = data["Cancer_Stage"] != "Stage 0"
+data = data.drop(["Patient_ID", "Cancer_Stage", "Year"], axis=1)
+data = pd.get_dummies(data)
+
+X = data.drop("cancer", axis=1)
+y = data["cancer"]
+
+# dividir dataset
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# crear modelo
+model = XGBClassifier()
+
+# entrenar
+model.fit(X_train, y_train)
+
+# predecir
+pred = model.predict(X_test)
+
+# evaluar
+accuracy = accuracy_score(y_test, pred)
+
+print("Accuracy:", accuracy)
+
+
+cm = confusion_matrix(y_test, pred)
+
+disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+disp.plot()
+
+plt.show()
